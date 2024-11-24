18fc2de32fd9c2e1a9e9c5d9a7278ad48050c736
ski1a <bilelzr8@gmail.com>
2024-11-24 15:54:14 +0100

    config

--- src/main/java/tn/pi/trainermanagement/InternalManagementApplication.java
+++ src/main/java/tn/pi/trainermanagement/InternalManagementApplication.java
@@ -2,8 +2,10 @@ package tn.pi.trainermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InternalManagementApplication {
 
     public static void main(String[] args) {

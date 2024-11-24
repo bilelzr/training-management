5f7991b9c31375762e519ade956aa50c056a685b
ski1a <bilelzr8@gmail.com>
2024-11-24 16:02:00 +0100

    conf

--- src/main/java/tn/pi/trainermanagement/InternalManagementApplication.java
+++ src/main/java/tn/pi/trainermanagement/InternalManagementApplication.java
@@ -2,15 +2,11 @@ package tn.pi.trainermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-=======
-
-@SpringBootApplication
->>>>>>> d783e54 (check)
 public class InternalManagementApplication {
 
     public static void main(String[] args) {
